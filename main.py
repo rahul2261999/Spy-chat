85d7ff57f2ead53d0e6712bdf509eb04cffe9b71
rahul2261999 <rahulsaini2261999@gmail.com>
2018-02-03 01:30:56 +0530

    spy detail

---
+++ main.py
@@ -0,0 +1,5 @@
+print 'lets start'
+spy_name =raw_input("enter name")
+spy_salutation = raw_input("What should we call you (Mr. or Ms.)")
+spy_name = spy_salutation + '' + spy_name
+print "alright" + " " + spy_name + '' + " I'd like to know a little bit more about."
